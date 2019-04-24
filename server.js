669089a09e2a1e8cb8d19ad48c148ea89c5d7f11
Christian Navolskyi <c.navolskyi@gmail.com>
2019-04-24 16:34:29 +0200

    Added heroku config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const users = require("./routes/api/user");
 
@@ -17,6 +18,17 @@ mongoose.connect(db, {useNewUrlParser: true})
 
 // Routes
 app.use("/api/user", users);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"))
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
